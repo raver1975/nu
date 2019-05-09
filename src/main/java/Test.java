93373c441118bdc1a926cde5f67996a9914141e3
Paul Klemstine <paul.klemstine@dodl.es>
2019-05-09 18:11:57 -0500

    sliders

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,5 @@
+import com.idrsolutions.image.JDeli;
+import com.idrsolutions.image.scale.SuperResolution;
 import org.tensorflow.Graph;
 import org.tensorflow.SavedModelBundle;
 import org.tensorflow.Session;
@@ -6,25 +8,42 @@ import org.tensorflow.Tensor;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.Panel;
 import java.awt.RenderingHints;
+import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.FloatBuffer;
+import java.util.Date;
 
 
 public class Test {
 
     static final int WIDTH = 128;
-    static final int SCAlE_WIDTH = 512;
-    static final float speed = .1f;
+    static final int SCAlE_WIDTH = 256;
+    static float speed = .1f;
+    private final int reset_after = 400;
+    private final boolean usesuperresolution = false;
 
-    float[][] input = new float[1][100];
-    float[] direction = new float[100];
+
+    private int num_images = 1;
+    float[][] input = new float[num_images][100];
+    float[][] direction = new float[num_images][100];
 
     public static void main(String[] args) throws IOException, InterruptedException {
         new Test();
@@ -34,37 +53,73 @@ public class Test {
 
         JFrame frame = new JFrame();
         frame.getContentPane().setLayout(new FlowLayout());
-        ImageIcon imageIcon = new ImageIcon(new BufferedImage(SCAlE_WIDTH, SCAlE_WIDTH, BufferedImage.TYPE_INT_RGB));
-        JLabel label = new JLabel(imageIcon);
-        label.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                resetImage();
-            }
+        ImageIcon[] imageIcon = new ImageIcon[num_images];
+        JLabel[] label = new JLabel[num_images];
+        GridLayout gridLayout = new GridLayout((int) Math.ceil(Math.sqrt(num_images)), (int) Math.ceil(Math.sqrt(num_images)) + 2);
+        GridLayout gridLayout1 = new GridLayout(25, 1);
+//        GridLayout gridLayout2 = new GridLayout(50, 1);
+        frame.getContentPane().setLayout(gridLayout);
+        JPanel[] panels = new JPanel[4];
+        for (int i = 0; i < 4; i++) {
+            panels[i] = new JPanel();
+            panels[i].setLayout(gridLayout1);
+        }
+        JSlider[] sliders = new JSlider[100];
+        for (int i = 0; i < 100; i++) {
+            sliders[i] = new JSlider();
+            sliders[i].setMinimum(-1000);
+            sliders[i].setMaximum((1000));
+            int finalI = i;
+            sliders[i].addChangeListener(new ChangeListener() {
+                @Override
+                public void stateChanged(ChangeEvent e) {
+                    input[0][finalI] = ((JSlider) e.getSource()).getValue() / 1000f;
+                    direction[0][finalI] = 0f;
+                }
+            });
+            panels[i % 4].add(sliders[i]);
+        }
+        for (int k = 0; k < num_images; k++) {
+            imageIcon[k] = new ImageIcon(new BufferedImage(SCAlE_WIDTH, SCAlE_WIDTH, BufferedImage.TYPE_INT_RGB));
+            label[k] = new JLabel(imageIcon[k]);
+            label[k].addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    resetImage();
+                }
 
-            @Override
-            public void mousePressed(MouseEvent e) {
+                @Override
+                public void mousePressed(MouseEvent e) {
 
-            }
+                }
 
-            @Override
-            public void mouseReleased(MouseEvent e) {
+                @Override
+                public void mouseReleased(MouseEvent e) {
 
-            }
+                }
 
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                System.out.println(e);
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    speed = .1f;
+//                    frame.setUndecorated(true);
+                }
 
-            }
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    speed = .02f;
+//                    frame.setUndecorated(false);
+//                    System.exit(0);
+                }
+            });
+            frame.getContentPane().add(label[k]);
+        }
+        for (int i = 0; i < 4; i++) {
+            frame.getContentPane().add(panels[i]);
+        }
 
-            @Override
-            public void mouseExited(MouseEvent e) {
-                System.exit(0);
-            }
-        });
-        frame.getContentPane().add(label);
-        frame.setUndecorated(true);
+        frame.setUndecorated(false);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation(dim.width / 2 - WIDTH / 2, dim.height / 2 - this.WIDTH / 2);
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -78,17 +133,25 @@ public class Test {
             resetImage();
             int cnt = 0;
             while (true) {
-                if (++cnt % 200 == 0) {
+                if (++cnt % reset_after == 0) {
                     resetImage();
                 }
-                for (int i = 0; i < 100; i++) {
-                    if (input[0][i] < -1f || input[0][i] > 1f) {
-                        direction[i] = -direction[i];
+                for (int k = 0; k < num_images; k++) {
+                    for (int i = 0; i < 100; i++) {
+                        if (input[k][i] < -1f || input[k][i] > 1f) {
+                            direction[k][i] = -direction[k][i];
+                        }
+                        input[k][i] += direction[k][i];
+                        direction[k][i] += ((float) (Math.random() * 2.0f) - 1f) / 50f;
                     }
-                    input[0][i] += direction[i];
-                    direction[i] += ((float) (Math.random() * 2.0f) - 1f) / 50f;
+                    normalize(direction[k], speed);
+
                 }
-                normalize(direction, speed);
+
+                for (int i = 0; i < 100; i++) {
+                    sliders[i].setValue((int) (input[0][i] * 1000f));
+                }
+
                 Tensor t = Tensor.create(input);
                 Tensor out = runAI(sess, t, "input_z", "generator/out");
 
@@ -98,20 +161,33 @@ public class Test {
 //                float discAr = discrData.array()[0];
 //                System.out.println(discAr);
 
-                BufferedImage image = scale(convert(out), SCAlE_WIDTH);
-                imageIcon.setImage(image);
-                label.repaint();
+                BufferedImage[] images;
+                images = usesuperresolution ? convert(out) : scale(convert(out), SCAlE_WIDTH);
+//                for (BufferedImage image:images) {
+//                }
+
+                //twoD.translate(SCAlE_WIDTH/2+SCAlE_WIDTH/3,SCAlE_WIDTH/2-SCAlE_WIDTH/3);
+//                drawClock(images[0],SCAlE_WIDTH/2+SCAlE_WIDTH/2,SCAlE_WIDTH/2+SCAlE_WIDTH/2);
+//                drawClock(images[2],SCAlE_WIDTH/2+SCAlE_WIDTH/2,SCAlE_WIDTH/2-SCAlE_WIDTH/2);
+//                drawClock(images[1],SCAlE_WIDTH/2-SCAlE_WIDTH/2,SCAlE_WIDTH/2+SCAlE_WIDTH/2);
+//                drawClock(images[3],SCAlE_WIDTH/2-SCAlE_WIDTH/2,SCAlE_WIDTH/2-SCAlE_WIDTH/2);
+                for (int k = 0; k < num_images; k++) {
+                    imageIcon[k].setImage(usesuperresolution ? SuperResolution.scale2x(images[k]) : images[k]);
+                    label[k].repaint();
+                }
             }
 
         }
     }
 
     private void resetImage() {
-        for (int i = 0; i < 100; i++) {
-            input[0][i] = (float) (Math.random() * 2.0f - 1f);
-            direction[i] = (float) ((Math.random() * 2.0f - 1f));
+        for (int k = 0; k < num_images; k++) {
+            for (int i = 0; i < 100; i++) {
+                input[k][i] = (float) (Math.random() * 2.0f - 1f);
+                direction[k][i] = (float) ((Math.random() * 2.0f - 1f));
+            }
+            normalize(direction[k], speed);
         }
-        normalize(direction, speed);
     }
 
 
@@ -133,39 +209,64 @@ public class Test {
     }
 
 
-    public BufferedImage convert(Tensor out) {
-        FloatBuffer imageData = FloatBuffer.allocate(128 * 128 * 3);
+    public BufferedImage[] convert(Tensor out) {
+        FloatBuffer imageData = FloatBuffer.allocate(num_images * 128 * 128 * 3);
         out.writeTo(imageData);
         imageData.rewind();
-        // fill rgbArray for BufferedImage
-        int[] rgbArray = new int[WIDTH * WIDTH];
-        int r, g, b, i = 0;
-        for (int y = WIDTH - 1; y > -1; --y) {
-            for (int x = 0; x < WIDTH; ++x) {
-                r = (int) (((imageData.get() + 1f) * 255) / 2f) << 16;
-                g = (int) (((imageData.get() + 1f) * 255) / 2f) << 8;
-                b = (int) (((imageData.get() + 1f) * 255) / 2f);
-                i = ((WIDTH - 1) - y) * WIDTH + x;
-                rgbArray[i] = r + g + b;
+        BufferedImage[] images = new BufferedImage[num_images];
+        float[] min = new float[num_images];
+        float[] max = new float[num_images];
+        float[] dif = new float[num_images];
+        for (int k = 0; k < num_images; k++) {
+            min[k] = Float.MAX_VALUE;
+            max[k] = Float.MIN_VALUE;
+        }
+        for (int k = 0; k < num_images; k++) {
+            // fill rgbArray for BufferedImage
+            for (int y = WIDTH - 1; y > -1; --y) {
+                for (int x = 0; x < WIDTH; ++x) {
+                    min[k] = Math.min(min[k], imageData.get());
+                    max[k] = Math.max(max[k], imageData.get());
+                    dif[k] = max[k] - min[k];
+                }
             }
         }
 
-        // create and save image
-        BufferedImage image = new BufferedImage(
-                WIDTH, WIDTH, BufferedImage.TYPE_INT_RGB
-        );
-        image.setRGB(0, 0, WIDTH, WIDTH, rgbArray, 0, WIDTH);
-        return image;
+        imageData.rewind();
+        for (int k = 0; k < num_images; k++) {
+            int r, g, b, i = 0;
+            int[] rgbArray = new int[WIDTH * WIDTH];
+            for (int y = WIDTH - 1; y > -1; --y) {
+                for (int x = 0; x < WIDTH; ++x) {
+                    r = (int) ((imageData.get() - min[k]) * 255f / dif[k]) << 16;
+                    g = (int) ((imageData.get() - min[k]) * 255f / dif[k]) << 8;
+                    b = (int) ((imageData.get() - min[k]) * 255f / dif[k]);
+                    i = ((WIDTH - 1) - y) * WIDTH + x;
+                    rgbArray[i] = r + g + b;
+                }
+            }
+
+            // create and save image
+            BufferedImage image = new BufferedImage(WIDTH, WIDTH, BufferedImage.TYPE_INT_RGB);
+            image.setRGB(0, 0, WIDTH, WIDTH, rgbArray, 0, WIDTH);
+            images[k] = image;
+
+        }
+        return images;
     }
 
-    public BufferedImage scale(BufferedImage original, int size) {
-        BufferedImage resized = new BufferedImage(size, size, original.getType());
-        Graphics2D g = resized.createGraphics();
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-                RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g.drawImage(original, 0, 0, size, size, 0, 0, original.getWidth(),
-                original.getHeight(), null);
-        g.dispose();
+    public BufferedImage[] scale(BufferedImage[] originals, int size) {
+        BufferedImage[] resized = new BufferedImage[num_images];
+        for (int k = 0; k < num_images; k++) {
+            BufferedImage resize = new BufferedImage(size, size, originals[k].getType());
+            Graphics2D g = resize.createGraphics();
+            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                    RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            g.drawImage(originals[k], 0, 0, size, size, 0, 0, originals[k].getWidth(),
+                    originals[k].getHeight(), null);
+            g.dispose();
+            resized[k] = resize;
+        }
         return resized;
     }
 
@@ -181,5 +282,39 @@ public class Test {
         for (int i = 0; i < input.length; i++) {
             input[i] = (((input[i] - min) / (max - min)) * 2f - 1f) * scale;
         }
+    }
+
+    public void drawClock(BufferedImage image, int x, int y) {
+        Date date = new Date();
+        int seconds = date.getSeconds();
+        int minutes = date.getMinutes();
+        int hours = date.getHours();
+
+        Graphics2D twoD = (Graphics2D) image.getGraphics();
+        twoD.setColor(new Color(255, 0, 0, 199));
+//        twoD.translate(SCAlE_WIDTH/2+SCAlE_WIDTH/3,SCAlE_WIDTH/2-SCAlE_WIDTH/3);
+        twoD.translate(x, y);
+
+//        Drawing the hour markers
+//        for(int i=0; i<12; i++) {
+//            twoD.drawLine(0, -SCAlE_WIDTH/4, 0, -SCAlE_WIDTH/2);
+//            twoD.rotate(Math.PI/6);
+//        }
+        twoD.rotate(seconds * Math.PI / 30);
+        twoD.setStroke(new BasicStroke(1));
+        twoD.setColor(new Color(255, 255, 0, 199));
+        twoD.drawLine(0, 0, 0, -SCAlE_WIDTH / 6);
+
+        twoD.rotate(2 * Math.PI - seconds * Math.PI / 30);
+        twoD.rotate(minutes * Math.PI / 30);
+        twoD.setStroke(new BasicStroke(3));
+        twoD.setColor(new Color(0, 0, 255, 199));
+        twoD.drawLine(0, 0, 0, -SCAlE_WIDTH / 4);
+
+        twoD.rotate(2 * Math.PI - minutes * Math.PI / 30);
+        twoD.rotate(hours * Math.PI / 6);
+        twoD.setStroke(new BasicStroke(6));
+        twoD.setColor(new Color(255, 0, 0, 199));
+        twoD.drawLine(0, 0, 0, -SCAlE_WIDTH / 5);
     }
 }
